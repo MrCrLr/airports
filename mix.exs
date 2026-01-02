b7518188e78121be0f1c2076695b69242c840589
Nicholas Stevens <nichstev@gmail.com>
2026-01-02 16:37:10 +0100

    Add ExCoveralls to check test coverage

--- mix.exs
+++ mix.exs
@@ -4,13 +4,27 @@ defmodule Airports.MixProject do
 
   def project do
     [
-      app:             :airports,
-      version:         "0.0.1",
-      name:            "Airports",
-      escript:         escript_config(),
-      elixir:          "~> 1.19",
-      start_permanent: Mix.env() == :prod,
-      deps:            deps()
+      app:               :airports,
+      version:           "0.0.1",
+      name:              "Airports",
+      escript:           escript_config(),
+      elixir:            "~> 1.19",
+      build_embedded:    Mix.env() == :prod,
+      start_permanent:   Mix.env() == :prod,
+      test_coverage:     [tool: ExCoveralls],
+      deps:              deps()
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls:             :test,
+        "coveralls.detail":    :test,
+        "coveralls.post":      :test,
+        "coveralls.html":      :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
@@ -27,7 +41,8 @@ defmodule Airports.MixProject do
       { :req,         "~> 0.5.16" },
       { :ex_doc,      "~> 0.34"   },
       { :earmark,     "~> 1.4.47" },
-      { :stream_data, "~> 1.2", only: :test }
+      { :stream_data, "~> 1.2", only: :test },
+      {:excoveralls, "~> 0.18", only: :test }
     ]
   end
 
